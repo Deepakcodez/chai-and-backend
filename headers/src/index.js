f51f65ab9c397aca4f4be11073d7448157986aef
Deepak <deepak2603dk@gmail.com>
2023-11-13 03:01:20 +0530

    http headers

---
+++ headers/src/index.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const server = http.createServer((req,res)=>{
+
+    let data = {x:5};
+
+    console.log('>>>>>>>>>>>', req.url)
+    console.log('>>>>>>>>>>>', "server started")
+
+    // resp.end(JSON.stringify({
+    //     message:"hello world"
+    // }))
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('demo','demoThing')
+    // res.setHeader('Content-Type','application/json')
+    // res.end(JSON.stringify(data))
+    res.setHeader('Content-Type','text/html')
+    res.end('<h1>hello</h1>');
+})
+
+server.listen(5000, () => {
+    console.log('Server is listening on port 5000');
+});
