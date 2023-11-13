8751c28acbc85b9b58080b58e995f17b259f2b20
Deepak <deepak2603dk@gmail.com>
2023-11-13 17:35:40 +0530

    version 2

--- renderhtmlfile/src/index.js
+++ renderhtmlfile/src/index.js
@@ -6,6 +6,16 @@ const port = 5000
 const publicPath = path.join(__dirname,'public')
 console.log('>>>>>>>>>>>', publicPath)
 
-app.use(express.static(publicPath))
+// app.use(express.static(publicPath))
+
+app.get('/',(req,resp)=>{
+  resp.sendFile(`${publicPath}/index.html`)
+})
+app.get('/about',(req,resp)=>{
+  resp.sendFile(`${publicPath}/about.html`)
+})
+app.get('*',(req,resp)=>{
+  resp.sendFile(`${publicPath}/errPage.html`)
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
