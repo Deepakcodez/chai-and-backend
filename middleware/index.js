6819eef9fec9a518dec61636b8aaf85ee9ada889
Deepak <deepak2603dk@gmail.com>
2023-11-14 16:48:45 +0530

    sending data through body

--- middleware/index.js
+++ middleware/index.js
@@ -2,10 +2,12 @@ const { log } = require('console');
 const express = require('express')
 const fs = require('fs')
 const app = express()
-const port = 3000
+const port = 8000
 const data = JSON.parse(fs.readFileSync('data.json','utf-8'));
 const products = data.products;
 
+app.use(express.json())
+app.use(express.urlencoded())
 
 // middleware 
 app.use((req,res,next)=>{
@@ -13,22 +15,37 @@ app.use((req,res,next)=>{
  next()
 })
 
-const auth=(req,res,next)=>{
-   if(req.query.password==123){
-    console.log('>>>>>>>>>>>', req.query)
-    next()
-   }
-   else{
-    res.sendStatus(401)
-   }
-}
+
+// auth middleware for auth in which send data throug query
+// const auth=(req,res,next)=>{
+//    if(req.query.password==123){
+//     console.log('>>>>>>>>>>>', req.query)
+//     next()
+//    }
+//    else{
+//     res.sendStatus(401)
+//    }
+// }
+
+// taking data through body but express wont alow to read body data so we need express moddleware
+const auth = (req, res, next) => {
+    if (req.body.password === '123') {
+       console.log('Authentication successful');
+       next();
+    } else {
+       console.log('Authentication failed');
+       res.sendStatus(401);
+    }
+ };
+ 
 
 
 
 // api endpoint
-app.get('/', auth , (req, res) => res.json(products))
+app.get('/',auth,  (req, res) => {
+    res.json(products)})
 
-app.post('/',(req,res)=>{
+app.post('/',auth,(req,res)=>{
     res.send({type:"post"})
 })
 app.put('/',(req,res)=>{
